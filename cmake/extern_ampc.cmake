2e0aa74474da89e1c003896bbedff32b76f775c6
Mat Haskell <mhaskell9@gmail.com>
2025-08-18 19:57:58 -0600

    Shared library version builds properly

---
+++ cmake/extern_ampc.cmake
@@ -0,0 +1,8 @@
+FetchContent_Declare(
+  ampc_ext
+  # TODO: change git info when it becomes public
+  GIT_REPOSITORY github:byu-rad-lab/affine_mpc_dev
+  GIT_TAG        origin/no_python
+)
+FetchContent_MakeAvailable(ampc_ext)
+

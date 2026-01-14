0c97673c9c1b42fa7e0467d1ab70fd04fcfbad2e
Mat Haskell <mhaskell9@gmail.com>
2026-01-13 21:05:47 -0700

    Fixed cmake issues

--- cmake/extern_ampc.cmake
+++ cmake/extern_ampc.cmake
@@ -2,6 +2,5 @@ FetchContent_Declare(ampc_extern
   # TODO: change git info when it becomes public
   GIT_REPOSITORY github:byu-rad-lab/affine_mpc_dev
   GIT_TAG        origin/main
-  # GIT_TAG        74f28cf
 )
 FetchContent_MakeAvailable(ampc_extern)

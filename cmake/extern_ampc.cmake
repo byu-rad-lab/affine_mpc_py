5cdd971d3c6b7fa5d6b3bcba92b9739ff02f82e6
Mat Haskell <mhaskell9@gmail.com>
2026-03-31 14:49:07 -0600

    Update cmake variables to match c++

--- cmake/extern_ampc.cmake
+++ cmake/extern_ampc.cmake
@@ -1,11 +1,11 @@
 FetchContent_Declare(ampc_extern
-  # TODO: change git info when it becomes public
+  # TODO: change git tag once there is a release
   GIT_REPOSITORY https://github.com/byu-rad-lab/affine_mpc
   GIT_TAG        origin/main
 )
 
-set(affine_mpc_BUILD_TESTS OFF CACHE BOOL "" FORCE)
-set(affine_mpc_BUILD_EXAMPLE OFF CACHE BOOL "" FORCE)
-set(affine_mpc_BINDINGS ON CACHE BOOL "" FORCE)
+set(AFFINE_MPC_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+set(AFFINE_MPC_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+set(AFFINE_MPC_BINDINGS ON CACHE BOOL "" FORCE)
 
 FetchContent_MakeAvailable(ampc_extern)
